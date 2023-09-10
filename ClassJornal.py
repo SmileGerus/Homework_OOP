ebf7bf80214b89daf9ca1a05ec6a4286c92eccc2
Artem Gerus <gerus.kz@gmail.com>
2023-09-11 01:58:44 +0300

    task 4

--- ClassJornal.py
+++ ClassJornal.py
@@ -1,3 +1,14 @@
+def mean_peoples_grades(peoples: list, course: str):
+    sum_mean = 0
+    len_mean = 0
+    for people in peoples:
+        sum_mean += sum(people.grades[course])
+        len_mean += len(people.grades[course])
+    if isinstance(people, Student):
+        print(f'Общая средняя оценка студентов: {sum_mean / len_mean:.1f}')
+    else:
+        print(f'Общая средняя оценка лекторов: {sum_mean / len_mean:.1f}')
+
 def mean(self):
     if len(self.grades.values()) == 0:
         return 0
@@ -28,40 +39,40 @@ class Student:
         if not isinstance(other, Lecturer):
             print('Сравнение возможно лишь с лекторами')
         if mean(self) < mean(other):
-            print('Студенты лузеры!')
+            print(f'{self.name} лузер!')
             return mean(self)
         else:
-            print('Студенты рулят!')
+            print(f'{self.name} рулит!')
             return mean(self)
 
     def __gt__(self, other):
         if not isinstance(other, Lecturer):
             print('Сравнение возможно лишь с лекторами')
         if mean(self) > mean(other):
-            print('Студенты рулят!')
+            print(f'{self.name} рулит!')
             return mean(self)
         else:
-            print('Студенты лузеры!')
+            print(f'{self.name} лузер!')
             return mean(self)
 
     def __le__(self, other):
         if not isinstance(other, Lecturer):
             print('Сравнение возможно лишь с лекторами')
         if mean(self) <= mean(other):
-            print('Студенты лузеры!')
+            print(f'{self.name} лузер!')
             return mean(self)
         else:
-            print('Студенты рулят!')
+            print(f'{self.name} рулит!')
             return mean(self)
 
     def __ge__(self, other):
         if not isinstance(other, Lecturer):
             print('Сравнение возможно лишь с лекторами')
         if mean(self) >= mean(other):
-            print('Студенты рулят!')
+            print(f'{self.name} рулит!')
             return mean(self)
         else:
-            print('Студенты лузеры!')
+            print(f'{self.name} лузер!')
             return mean(self)
 
     def lecturer_evaluations(self, lecturer, course, grade: int):
@@ -73,6 +84,7 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             print('Ошибка')
+
 
 
 class Mentor:
@@ -102,40 +114,40 @@ class Lecturer(Mentor):
         if not isinstance(other, Student):
             print('Сравнение возможно лишь со студентами')
         if mean(self) < mean(other):
-            print('Лекторы лузеры!')
+            print(f'{self.name} лузер!')
             return mean(self)
         else:
-            print('Лекторы рулят!')
+            print(f'{self.name} рулит!')
             return mean(self)
 
     def __gt__(self, other):
         if not isinstance(other, Student):
             print('Сравнение возможно лишь со студентами')
         if mean(self) > mean(other):
-            print('Лекторы рулят!')
+            print(f'{self.name} рулит!')
             return mean(self)
         else:
-            print('Лекторы лузеры!')
+            print(f'{self.name} лузер!')
             return mean(self)
 
     def __le__(self, other):
         if not isinstance(other, Student):
             print('Сравнение возможно лишь со студентами')
         if mean(self) <= mean(other):
-            print('Лекторы лузеры!')
+            print(f'{self.name} лузер!')
             return mean(self)
         else:
-            print('Лекторы рулят!')
+            print(f'{self.name} рулит!')
             return mean(self)
 
     def __ge__(self, other):
         if not isinstance(other, Student):
             print('Сравнение возможно лишь со студентами')
         if mean(self) >= mean(other):
-            print('Лекторы рулят!')
+            print(f'{self.name} рулит!')
             return mean(self)
         else:
-            print('Лекторы лузеры!')
+            print(f'{self.name} лузер!')
             return mean(self)
 
     def __str__(self):
@@ -152,35 +164,67 @@ class Reviewer(Mentor):
 
 
 sergey_brin = Student('Sergey', 'Brin', 'male')
-sergey_brin.courses_in_progress += ['Python']
+sergey_brin.courses_in_progress += ['C++']
+sergey_brin.courses_in_progress += ['TeleBot']
 sergey_brin.finished_courses += ["Golang"]
 
 larry_page = Student('Larry', 'Page', 'male')
 larry_page.courses_in_progress += ['C++']
+larry_page.courses_in_progress += ['Python']
 larry_page.finished_courses += ["Golang"]
 
 steve_jobs = Lecturer('Steve', 'Jobs')
 steve_jobs.courses_attached += ['Python']
 steve_jobs.courses_attached += ['Swift']
+steve_jobs.courses_attached += ['CSS']
+
+elon_mask = Lecturer('Elon', 'Mask')
+elon_mask.courses_attached += ['PHP']
+elon_mask.courses_attached += ['HTML']
+elon_mask.courses_attached += ['CSS']
+elon_mask.courses_attached += ['JS']
 
 bill_gates = Reviewer('Bill', 'Gates')
 bill_gates.courses_attached += ['C++']
 
+
+pavel_durov = Reviewer('Pavel', 'Durov')
+pavel_durov.courses_attached += ['TeleBot']
+
 sergey_brin.lecturer_evaluations(steve_jobs, 'Python', 5)
 sergey_brin.lecturer_evaluations(steve_jobs, 'Python', 10)
 sergey_brin.lecturer_evaluations(steve_jobs, 'Python', 10)
-
 sergey_brin.lecturer_evaluations(steve_jobs, 'Swift', 10)
 sergey_brin.lecturer_evaluations(steve_jobs, 'Swift', 10)
+sergey_brin.lecturer_evaluations(steve_jobs, 'CSS', 10)
+
+larry_page.lecturer_evaluations(elon_mask, 'PHP', 2)
+larry_page.lecturer_evaluations(elon_mask, 'HTML', 1)
+larry_page.lecturer_evaluations(elon_mask, 'CSS', 4)
+larry_page.lecturer_evaluations(elon_mask, 'JS', 6)
+
 
 bill_gates.rate_hw(larry_page, 'C++', 10)
 bill_gates.rate_hw(larry_page, 'C++', 10)
 bill_gates.rate_hw(larry_page, 'C++', 10)
+bill_gates.rate_hw(sergey_brin, 'C++', 10)
+bill_gates.rate_hw(sergey_brin, 'C++', 10)
+bill_gates.rate_hw(sergey_brin, 'C++', 10)
+
+pavel_durov.rate_hw(sergey_brin, 'TeleBot', 9)
+pavel_durov.rate_hw(sergey_brin, 'TeleBot', 5)
+pavel_durov.rate_hw(sergey_brin, 'TeleBot', 10)
 
 print(steve_jobs.grades)
 print(larry_page.grades)
+print(sergey_brin.grades)
+print(elon_mask.grades)
 print()
 print(bill_gates)
+print()
+print(pavel_durov)
+print()
+print(elon_mask)
 print()
 print(steve_jobs)
 print()
@@ -191,3 +235,11 @@ print()
 print(larry_page >= steve_jobs)
 print()
 print(steve_jobs <= larry_page)
+print()
+print(elon_mask < sergey_brin)
+print()
+print(sergey_brin > elon_mask)
+print()
+mean_peoples_grades([larry_page, sergey_brin], 'C++')
+print()
+mean_peoples_grades([steve_jobs, elon_mask], 'CSS')
